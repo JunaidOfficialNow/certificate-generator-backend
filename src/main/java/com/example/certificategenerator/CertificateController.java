a85d882b24f00b67f6fb81b5340f373463b27968
Junaid <junaidofficialnow@gmail.com>
2024-04-18 15:40:46 +0530

    feat: created generate  and download certificate routes

--- src/main/java/com/example/certificategenerator/CertificateController.java
+++ src/main/java/com/example/certificategenerator/CertificateController.java
@@ -1,16 +1,49 @@
 package com.example.certificategenerator;
 
 
+import com.ironsoftware.ironpdf.PdfDocument;
+import com.ironsoftware.ironpdf.edit.PageSelection;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/certificates")
+import java.io.File;
+import java.nio.file.Paths;
+
+@RestController
+@RequestMapping("/certificates")
 public class CertificateController {
 
     @PostMapping("/generate")
-    public String generateCertificate(@RequestBody() String  body) {
+    public ResponseEntity<String> generateCertificate() throws Exception {
+        String templateNo = "1";
+        String templatePath = "src/main/resources/templates/" + templateNo + ".pdf";
+        PdfDocument pdfDoc = PdfDocument.fromFile(Paths.get(templatePath));
+        pdfDoc.replaceText(PageSelection.firstPage(), "RecipientName", "Sample  name");
+        pdfDoc.saveAs(Paths.get("src/main/resources/outputs/sample.pdf"));
+        pdfDoc.close();
+        return ResponseEntity.ok("Success") ;
+    }
 
-        return  "Hello";
+    @GetMapping("/download")
+    public ResponseEntity<FileSystemResource> downloadPdf() {
+        String  FILE_PATH = "src/main/resources/outputs/sample.pdf";
+        File file = new File(FILE_PATH);
+        if (!file.exists()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.setContentDispositionFormData("attachment", file.getName());
+        headers.setContentLength(file.length());
+        FileSystemResource resource = new FileSystemResource(file);
+        return new ResponseEntity<>(resource, headers, HttpStatus.OK);
     }
 }
