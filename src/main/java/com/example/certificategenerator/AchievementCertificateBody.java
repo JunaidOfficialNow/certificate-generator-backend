727684ce9fe2350f3583863c6caea74020ece6ac
Junaid <junaidofficialnow@gmail.com>
2024-04-25 23:22:40 +0530

    version-1

--- src/main/java/com/example/certificategenerator/AchievementCertificateBody.java
+++ src/main/java/com/example/certificategenerator/AchievementCertificateBody.java
@@ -2,6 +2,6 @@ package com.example.certificategenerator;
 
 public class AchievementCertificateBody {
     public String name;
-    public  String representative1;
+    public String representative1;
     public String representative2;
 }
